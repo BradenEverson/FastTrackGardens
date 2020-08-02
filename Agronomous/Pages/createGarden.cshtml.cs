0580712d725588983e8c19152bb4c20a2da117e1
Braden Everson <bradeneverson@gmail.com>
2020-08-02 08:20:27 -0500

    Completed garden creation logic

--- Agronomous/Pages/createGarden.cshtml.cs
+++ Agronomous/Pages/createGarden.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Agronomous.Data;
+using Agronomous.Database;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,9 +13,11 @@ namespace Agronomous
     public class createGardenModel : PageModel
     {
         private readonly ApplicationDbContext db;
-        public createGardenModel(ApplicationDbContext db)
+        private readonly IPlantData plants;
+        public createGardenModel(ApplicationDbContext db, IPlantData plants)
         {
             this.db = db;
+            this.plants = plants;
         }
         public void OnGet()
         {
--- Agronomous/Pages/garden.cshtml.cs
+++ Agronomous/Pages/garden.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agronomous.Core;
+using Agronomous.Data;
+using Agronomous.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,17 @@ namespace Agronomous
 {
     public class gardenModel : PageModel
     {
+        private readonly IPlantData plants;
+        private readonly ApplicationDbContext db;
+        public List<plant> userGarden;
+        public gardenModel(IPlantData plants, ApplicationDbContext db)
+        {
+            this.plants = plants;
+            this.db = db;
+        }
         public void OnGet()
         {
+            userGarden = plants.aggregateGarden(Guid.Parse(db.Users.FirstOrDefault(r => r.UserName == User.Identity.Name).plantGuid));
         }
         public IActionResult OnPost()
         {
